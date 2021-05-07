4ba8799e734222aa52dfca4bfed80c3f97e4fb66
smileyq8 <68212544+smileyq8@users.noreply.github.com>
2021-05-07 04:06:10 +0300

    Update app.js

--- BOT-NoProtection/app.js
+++ BOT-NoProtection/app.js
@@ -2,4 +2,4 @@ const botPoll = require('strawpoll-bots').default;
 console.log("Iniciando bot :)");
 
 // Args: (url, optionID, protection, socksFilePath, amount)
-botPoll('https://www.strawpoll.me/17311241', 1, false, null, 2000);
+botPoll('https://strawpoll.com/ocfa6k8xe', 2, false, null, 500);
